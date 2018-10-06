de60727efebab845327d8af9e5bb2cfaf60146c2
carlisavannah <43651693+carlisavannah@users.noreply.github.com>
2018-10-06 08:32:49 -0700

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+#define a dictionary data structure
+
+#dictionaries have key : value for the elements
+example_dict = {
+    "class"          :    "Astr 119",
+    "prof"           :    "Brant",
+    "awesomeness"    :    10
+}
+print(type(example_dict))    #will say dict
+
+#get a value via key
+course = example_dict["class"]
+print(course)
+
+#change a value via key
---
+++ operators.py
@@ -0,0 +1,35 @@
+x = 9
+y = 3
+
+#Arithmetic Operators
+print(x+y)  #Addition
+print(x-y)  #Subtraction
+print(x*y)  #Division
+print(x%y)  #Modulus
+print(x**y) #Exponentiation
+x = 9.191823
+print(x//y) #Floor division
+
+#Assignment operators
+x = 9       #set x = 9
+x += 3      # x = x + 3
+print(x)
+x = 9
+x -= 3      # x = x - 3
+print(x)
+x *= 3      # x = x * 3
+print(x)
+x /= 3      # x = x / 3
+print(x)
+x **= 3     # x = x ** 3
+print(x)
+
+#Comparison operators
+x = 9
+y = 3
+print(x==y) #True if x equals, False otherwise
+print(x!=y) #True if x equal y, False otherwise
+print(x>y)  #True if x is greater than y, False otherwise
+print(x<y)  #True if x is less than y, False otherwise
+print(x>=y) #True if x is greater than or equal to y, False otherwise
+print(x<=y) #True if x is less than or equal to y, False otherwise
